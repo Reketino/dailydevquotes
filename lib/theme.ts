516cc489f603bcb263ad6e6ae56777a6e73f91c2
Reketino <bjornevensk8@gmail.com>
2026-04-01 10:10:43 +0200

    Feat: setup light theme in theme.ts

--- lib/theme.ts
+++ lib/theme.ts
@@ -5,5 +5,8 @@ export const themes = {
         background: "linear-gradient(135deg, #020617, #0f172a)",
         text: "#e5e7eb"
     },
-    light
+    light: {
+        background: "linear-gradient(135deg, #ffffff, #f1f5f9)",
+        text: "#020617"
+    }
 }
