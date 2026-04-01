54050bb71ceab5d54c4a9a0007dbcc72570435e3
Reketino <bjornevensk8@gmail.com>
2026-04-01 10:07:38 +0200

    Fix: typing

--- lib/theme.ts
+++ lib/theme.ts
@@ -2,7 +2,8 @@
 
 export const themes = {
     dark: {
-        backgground: "linear-gradient(135deg, #020617, #0f172a)",
+        background: "linear-gradient(135deg, #020617, #0f172a)",
         text: "#e5e7eb"
-    }
+    },
+    light
 }
