92e2f9a9ec0b3139602d7097b27c8f88727e6ac5
Reketino <bjornevensk8@gmail.com>
2026-04-04 00:02:55 +0200

    Style: formatting, UI, no logic change

--- lib/hash.ts
+++ lib/hash.ts
@@ -1,11 +1,9 @@
-
-
 export function hash(str: string): number {
-    let h = 0;
+  let h = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        h = (h << 5) - h + str.charCodeAt(i);
-        h |= 0;
-    }
-    return Math.abs(h);
+  for (let i = 0; i < str.length; i++) {
+    h = (h << 5) - h + str.charCodeAt(i);
+    h |= 0;
+  }
+  return Math.abs(h);
 }
