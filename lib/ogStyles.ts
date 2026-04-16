7d218a86e94d753311d3e1be3a0ccff1e865508d
Reketino <bjornevensk8@gmail.com>
2026-04-17 00:05:24 +0200

    Style: formatting, UI, no logic change

--- lib/ogStyles.ts
+++ lib/ogStyles.ts
@@ -5,25 +5,25 @@ export const flexColCenter = {
 };
 
 export const textBase = {
-    display: "flex",
+  display: "flex",
 };
 
 export const labelText = {
-    ...textBase,
-    fontSize: 16,
-    opacity: 0.6,
-    letterSpacing: "0.08em",
+  ...textBase,
+  fontSize: 16,
+  opacity: 0.6,
+  letterSpacing: "0.08em",
 };
 
 export const newsText = {
-    ...textBase,
-    fontSize: 20,
-    lineheight: 1.5,
-    wordWrap: "break-word"
+  ...textBase,
+  fontSize: 20,
+  lineheight: 1.5,
+  wordWrap: "break-word",
 };
 
 export const domainText = {
-    ...textBase,
-    fontSize: 14,
-    opacity: 0.5,
+  ...textBase,
+  fontSize: 14,
+  opacity: 0.5,
 };
