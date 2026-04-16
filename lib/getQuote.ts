67a61708523648c9af8b3ec3709e601efa4b1709
Reketino <bjornevensk8@gmail.com>
2026-04-17 00:12:32 +0200

    Style: formatting, UI, no logic change

--- lib/getQuote.ts
+++ lib/getQuote.ts
@@ -1,33 +1,32 @@
-import { hash } from "./hash"
+import { hash } from "./hash";
 
 export type Mood = "chaos" | "pain" | "fun" | "wisdom";
 
-  type Quote = string | { text: string; mood?: Mood };
+type Quote = string | { text: string; mood?: Mood };
 
-  const QUOTES_URL =
+const QUOTES_URL =
   "https://raw.githubusercontent.com/Reketino/dev-quotes/master/quotes.json";
 
-  export async function getQuote(user: string) {
-    const res = await fetch (QUOTES_URL, {
-        next: { revalidate: 86400 },
-    })
+export async function getQuote(user: string) {
+  const res = await fetch(QUOTES_URL, {
+    next: { revalidate: 86400 },
+  });
 
   const rawQuotes: Quote[] = await res.json();
 
   const day = Math.floor(Date.now() / 86400000);
   const index = hash(`${user}-${day}`) % rawQuotes.length;
 
-   const raw = rawQuotes[index];
+  const raw = rawQuotes[index];
 
-   const text =
+  const text =
     typeof raw === "string"
       ? raw
       : typeof raw?.text === "string"
         ? raw.text
         : "Code is hard. Life is harder.";
 
-  const mood: Mood = 
-  typeof raw === "object" && raw?.mood ? raw.mood : "chaos";
- 
-  return { text, mood}
-  }
+  const mood: Mood = typeof raw === "object" && raw?.mood ? raw.mood : "chaos";
+
+  return { text, mood };
+}
