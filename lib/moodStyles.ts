000e7cef61983639f855e46a709b5b47543c0ec3
Reketino <bjornevensk8@gmail.com>
2026-04-19 00:04:00 +0200

    Feat: setup shell for moodstyles

--- lib/moodStyles.ts
+++ lib/moodStyles.ts
@@ -4,3 +4,22 @@ export type MoodStyle = {
     glow: string;
     accent: string;
 };
+
+export const moodStyles: Record<Mood, MoodStyle> = {
+    chaos: {
+        glow: "",
+        accent: "",
+    },
+    pain: {
+        glow: "",
+        accent: "", 
+    },
+    fun: {
+         glow: "",
+        accent: "",
+    },
+    wisdom: {
+         glow: "",
+        accent: "",
+    },
+};
