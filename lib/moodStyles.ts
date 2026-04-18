cb2252d64b599248c122a555179e6981fadfd1b3
Reketino <bjornevensk8@gmail.com>
2026-04-19 00:04:46 +0200

    Style: formatting

--- lib/moodStyles.ts
+++ lib/moodStyles.ts
@@ -1,25 +1,25 @@
 import { Mood } from "./getQuote";
 
 export type MoodStyle = {
-    glow: string;
-    accent: string;
+  glow: string;
+  accent: string;
 };
 
 export const moodStyles: Record<Mood, MoodStyle> = {
-    chaos: {
-        glow: "",
-        accent: "",
-    },
-    pain: {
-        glow: "",
-        accent: "", 
-    },
-    fun: {
-         glow: "",
-        accent: "",
-    },
-    wisdom: {
-         glow: "",
-        accent: "",
-    },
+  chaos: {
+    glow: "",
+    accent: "",
+  },
+  pain: {
+    glow: "",
+    accent: "",
+  },
+  fun: {
+    glow: "",
+    accent: "",
+  },
+  wisdom: {
+    glow: "",
+    accent: "",
+  },
 };
