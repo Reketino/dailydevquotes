8756d205c54e2bcb4cf5bf93c0abc854cc629378
Reketino <bjornevensk8@gmail.com>
2026-07-03 00:01:52 +0200

    feat: added const json to getCachedNews.ts

--- lib/getCachedNews.ts
+++ lib/getCachedNews.ts
@@ -2,5 +2,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 export async function getCachedNews() {
-   const file = path.join(process.cwd(), "public/cache/news.json"); 
+   const file = path.join(process.cwd(), "public/cache/news.json");
+   
+   const json = await fs.readFile(file, "utf-8");
 }
