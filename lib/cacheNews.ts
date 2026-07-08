c09fff2044be0016845d4802ed5d5e4b532192b3
Reketino <bjornevensk8@gmail.com>
2026-07-09 00:02:22 +0200

    feat: setup const file

--- lib/cacheNews.ts
+++ lib/cacheNews.ts
@@ -4,4 +4,5 @@ import { getDevNews } from "./getDevNews";
 
 export async function cacheNews() {
     const news = await getDevNews();
+    const file = path.join(process.cwd(), "public/cache/news.json");
 }
