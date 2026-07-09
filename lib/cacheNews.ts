88d8499af83238f32be291f66cae12d602fb2dd6
Reketino <bjornevensk8@gmail.com>
2026-07-10 00:04:46 +0200

    feat: setup await fs logic iin cachenews

--- lib/cacheNews.ts
+++ lib/cacheNews.ts
@@ -5,4 +5,5 @@ import { getDevNews } from "./getDevNews";
 export async function cacheNews() {
     const news = await getDevNews();
     const file = path.join(process.cwd(), "public/cache/news.json");
+    await fs.writeFile(file, JSON.stringify(news, null, 2), "utf-8");
 }
